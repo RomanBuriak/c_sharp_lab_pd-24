06a0557363f891eea5573a099cff75f54075f68f
RomaB <roma.buriak01@gmail.com>
2019-09-24 19:27:52 +0300

    Add Lab 1
    Обчислити період коливання маятника заданої довжини

---
+++ Lab/Lab_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Лаба 1 (1 завдання)     
+//Обчислити період коливаня маятника заданої довжини
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length; // довжина
+
+            double pi = Math.PI;     // число "пі"
+            double g = 9.8;    // прискорення вільного падіння
+            double T;      // період коливань
+
+
+            do
+            {
+                Console.WriteLine("Задайте довжину маятника:");
+                length = Convert.ToDouble(Console.ReadLine());
+            } while (length < 0);
+
+            T = 2 * pi * Math.Sqrt(length / g);
+
+            Console.WriteLine("T = {0}", T);
+
+            Console.ReadKey();
+        }
+    }
+}
