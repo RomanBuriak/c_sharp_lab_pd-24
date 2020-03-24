677bf52b2b64de7aebe0ac4b8fe776fef4028c10
RomaB <roma.buriak01@gmail.com>
2020-03-25 01:04:45 +0200

    Lab 2 and 3

--- Lab_Sem_2/Lab_2/Form1.Designer.cs
+++ Lab_Sem_2/Lab_2/Form1.Designer.cs
@@ -28,19 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Timer1
+            // button2
             // 
-            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            this.button2.Font = new System.Drawing.Font("Calligraphic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(442, 325);
+            this.button2.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CANCEL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Cancel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calligraphic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(442, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(970, 622);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Old English Text MT", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
             this.Name = "Form1";
             this.Text = "Form";
             this.ResumeLayout(false);
@@ -48,8 +72,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Lab_Sem_2/Lab_2/Form1.cs
+++ Lab_Sem_2/Lab_2/Form1.cs
@@ -12,21 +12,155 @@ namespace Lab_2
 {
     public partial class Form1 : Form
     {
+        int counter = 0;
+        int i = 0;
+        Timer Timer1 = new Timer();
         public Form1()
         {
             InitializeComponent();
-            this.Timer1.Interval = 200;
-            this.Timer1.Tick += Timer1_Tick;
-            this.Timer1.Start();
+            this.MouseMove += Button_Move;
+            Text = " ";
+            if (Timer1.Enabled)
+            {
+                Timer1.Enabled = false;
+            }
+            else
+            {
+                Timer1.Interval = 1000;
+                Timer1.Tick += new EventHandler(Timer1_Tick);
+                Timer1.Start();
+            }
+
         }
 
-        string text = "12345";
-        bool on_or_off = true;
-        int i = 0;
+        
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
+            i++;
+            
+            if (this.Text == " ")
+            {
+                this.Text = "Натисніть кнопку “ОK”!!!";
+            }
+            else if (this.Text == "Натисніть кнопку “ОK”!!!")
+            {
+                this.Text = " ";
+            }
+            if (i == 15)
+            {
+                Timer1.Stop();
+            }
+        }
+
+        private void Button_Move(object sender, MouseEventArgs e)
+        {
+            int speed = 2;
+            int field = 30;
+            int cursor_x = Cursor.Position.X;
+            int cursor_y = Cursor.Position.Y;
+            int button_x = button1.Location.X;
+            int button_y = button1.Location.Y;
+            //->
+            if (cursor_x - field >= button_x)
+            {
+                button1.Location = new System.Drawing.Point(button_x + speed, button_y);
+            }
+            //<-
+            if (cursor_x + button1.Width + field <= button_x)
+            {
+                button1.Location = new System.Drawing.Point(button_x - speed, button_y);
+            }
+            //down
+            if (cursor_y - field >= button_x)
+            {
+                button1.Location = new System.Drawing.Point(button_x, button_y + speed);
+            }
+            //up
+            if (cursor_y + button1.Height + field == button_x)
+            {
+                button1.Location = new System.Drawing.Point(button_x, button_y + speed);
+            }
+            //down and right
+            
+            if (button1.Size.Width < 1 || button1.Size.Height < 1)
+                this.Text = "Кнопка “Ок” не може бути натиснута";
+            bool move = false;
+
+            bool Limit_X = e.X > button_x - field && e.X < button_x;//->
+            bool Limit_Y = e.Y > button_y - field && e.Y < button_y;//down
+            bool Limit_And_Size_Button_X = e.X > button_x - field + 100 && e.X < button_x + 100;//->
+            bool Limit_And_Size_Button_Y = e.Y > button_y - field + 50 && e.Y < button_y + 50;//
+            bool Limit_X_Revers = e.X > button_x && e.X < button_x - field + 100;//
+            bool Limit_Y_Revers = e.Y > button_y && e.Y < button_y - field + 50;//
+            if (Limit_X && Limit_Y)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x + speed, button_y + speed);
+                move = true; counter++;
+            }
+            else if (Limit_And_Size_Button_X && Limit_And_Size_Button_Y)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x - speed, button_y - speed);
+                move = true; counter++;
+            }
+            else if (Limit_X && Limit_And_Size_Button_Y)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x + speed, button_y - speed);
+                move = true; counter++;
+            }
+            else if (Limit_And_Size_Button_X && Limit_Y)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x - speed, button_y + speed);
+                move = true; counter++;
+            }
+            else if (Limit_X_Revers && Limit_Y)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x, button_y + speed);
+                move = true; counter++;
+            }
+            else if (Limit_X_Revers && Limit_And_Size_Button_Y)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x, button_y - speed);
+                move = true; counter++;
+            }
+            else if (Limit_And_Size_Button_X && Limit_Y_Revers)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x - speed, button_y);
+                move = true; counter++;
+            }
+            else if (Limit_X && Limit_Y_Revers)
+            {
+                this.button1.Location = new System.Drawing.Point(button_x + speed, button_y);
+                move = true; counter++;
+            }
+
+
+
+            //Край форми
+            field = 50;
+            if (button1.Location.X > this.Size.Width - 80)
+                button_x -= field;
+            else if (button1.Location.Y > this.Size.Height - 80)
+                button_y -= field;
+            else if (button1.Location.Y < 0)
+                button_y += field;
+            else if (button1.Location.X < 0)
+                button_x += field;
+            this.button1.Location = new System.Drawing.Point(button_x, button_y);
+
+            //Зменшння кнопки
+            if (move && counter % 10 == 0)
+            {
+                this.button1.Size = new System.Drawing.Size(button1.Width, --button1.Height);
+            }
 
         }
+
+
+        private void Button_Cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
     }
 }
